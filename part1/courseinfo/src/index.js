8b9a3f7ff1dba175df928508b607c52c79fd5411
sagor9116 <sagor.roy9116@gmail.com>
2021-02-16 00:34:54 +0600

    Component state, event handlers

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,68 +1,42 @@
-import React from 'react';
+//Component state, event handlers
+
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 
 
-const Header = (props) => {
-  console.log(props)
+const Display = ({counter}) => {
   return(
     <div>
-      <h1>{props.course}</h1>
+      <h1>{counter}</h1>
     </div>
   )
 }
 
-const Part = (props) => {
-  console.log(props)
+const Button = ({handleClick,text}) => {
   return(
-    <div>
-      <p>{props.parts} {props.exercises}</p>
-    </div>
+    <button onClick={handleClick}>
+        {text}
+    </button>
   )
 }
-
-const Content = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <Part parts = {props.parts[0].name} exercises = {props.parts[0].exercises} />
-      <Part parts = {props.parts[1].name} exercises = {props.parts[1].exercises} />
-      <Part parts = {props.parts[2].name} exercises = {props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
-    </div>
-  )
-}
-
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter,setCounter] = useState(0)
+  const increasedByOne = () => {
+    setCounter(counter + 1)
+  }
+  const decreasedByOne = () => {
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    setCounter(0)
   }
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts = {course.parts} />
-      <Total parts = {course.parts} />
+      <Display counter ={counter} />
+      <Button handleClick = {increasedByOne} text = "plus" />
+      <Button handleClick = {decreasedByOne} text="minus"/>
+      <Button handleClick={setToZero} text="zero" />
     </div>
   );
 }
